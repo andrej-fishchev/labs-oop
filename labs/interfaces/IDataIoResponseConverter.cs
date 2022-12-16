054008975f13d9acd74bbe964512690a6808f763
rejchev <hendlerq@yandex.ru>
2022-12-16 14:14:50 +0500

    update file structure

--- labs/interfaces/IDataIoResponseConverter.cs
+++ labs/interfaces/IDataIoResponseConverter.cs
@@ -1,10 +1,8 @@
-namespace labs.interfaces
+namespace labs.interfaces;
+
+public interface IDataIoResponseConverter<TO>
 {
-    
-    public interface IDataIoResponseConverter<TO>
-    {
-        public IDataIoResponse<TO> Convert<T>(IDataIoResponse<T> data, DataConverter<T, TO> converter);
-    }
-    
-    public delegate TO DataConverter<in TI, out TO>(TI data, out string? error);
+    public IDataIoResponse<TO> Convert<T>(IDataIoResponse<T> data, DataConverter<T, TO> converter);
 }
+    
+public delegate TO DataConverter<in TI, out TO>(TI data, out string? error);
--- labs/interfaces/IDataIoValidator.cs
+++ labs/interfaces/IDataIoValidator.cs
@@ -1,10 +1,8 @@
-namespace labs.interfaces
-{
-    public interface IDataIoValidator<T>
-    {
-        public bool Validate(IDataIoResponse<T> data);
-    }
-    
-    public delegate bool ValidateDataExpression<in T>(T data);    
-}
+namespace labs.interfaces;
 
+public interface IDataIoValidator<T>
+{
+    public bool Validate(IDataIoResponse<T> data);
+}
+    
+public delegate bool ValidateDataExpression<in T>(T data);
