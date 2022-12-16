6af563082f3682aeab605ea5046335b936635acf
rejchev <hendlerq@yandex.ru>
2022-12-16 14:15:20 +0500

    update file structure x2

--- labs/IO/DataIoConverter.cs
+++ labs/IO/DataIoConverter.cs
@@ -1,31 +1,30 @@
 using labs.interfaces;
 
-namespace labs.IO
+namespace labs.IO;
+
+public class DataIoConverter<TOut> :
+    IDataIoResponseConverter<TOut>
 {
-    public class DataIoConverter<TOut> :
-        IDataIoResponseConverter<TOut>
+    public IDataIoResponse<TOut> OuterData { get; set; } 
+            
+    public DataIoConverter(IDataIoResponse<TOut> emptyObject)
     {
-        public IDataIoResponse<TOut> OuterData { get; set; } 
+        OuterData = emptyObject;
+    }
+
+    public IDataIoResponse<TOut> Convert<T>(IDataIoResponse<T> data, DataConverter<T, TOut> converter)
+    {
+        OuterData.Code = data.Code;
             
-        public DataIoConverter(IDataIoResponse<TOut> emptyObject)
-        {
-            OuterData = emptyObject;
-        }
+        if (data.Error != null)
+            OuterData.Error = data.Error;
 
-        public IDataIoResponse<TOut> Convert<T>(IDataIoResponse<T> data, DataConverter<T, TOut> converter)
-        {
-            OuterData.Code = data.Code;
-            
-            if (data.Error != null)
-                OuterData.Error = data.Error;
-
-            if (data.Data == null)
-                return OuterData;
-
-            OuterData.Data = converter.Invoke(data.Data, out var error);
-            OuterData.Error = error;
-
+        if (data.Data == null)
             return OuterData;
-        }
+
+        OuterData.Data = converter.Invoke(data.Data, out var error);
+        OuterData.Error = error;
+
+        return OuterData;
     }
 }
