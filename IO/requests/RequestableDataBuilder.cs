3377e3893d097ca8776e683ebee71e2e95464e3f
rejchev <hendlerq@yandex.ru>
2022-12-29 05:35:35 +0500

    add requestable data builder

---
+++ IO/requests/RequestableDataBuilder.cs
@@ -0,0 +1,17 @@
+namespace IO.requests;
+
+public abstract class RequestableDataBuilder<TIn, TOut> :
+    IBuildable<IRequestableData<TIn, TOut>>
+{
+    private IRequestableData<TIn, TOut> entity;
+
+    public RequestableDataBuilder(IRequestableData<TIn, TOut> value)
+    {
+        entity = value;
+    }
+
+    public IRequestableData<TIn, TOut> Build()
+    {
+        return entity;
+    }
+}
