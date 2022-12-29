04a6d53b25d7392dab704466a78497f8d12bbf95
rejchev <hendlerq@yandex.ru>
2022-12-29 05:36:06 +0500

    add console req. builder

---
+++ IO/requests/ConsoleDataRequestBuilder.cs
@@ -0,0 +1,25 @@
+namespace IO.requests;
+
+public class ConsoleDataRequestBuilder<TOut> :
+    RequestableDataBuilder<string?, TOut>
+{
+    public ConsoleDataRequestBuilder(ConsoleDataRequest<TOut> value) : 
+        base(value)
+    { }
+
+    public virtual ConsoleDataRequestBuilder<TOut> Display(string value)
+    {
+        ((ConsoleDataRequest<TOut>)Build())
+            .DisplayMessage = value;
+
+        return this;
+    }
+    
+    public virtual ConsoleDataRequestBuilder<TOut> RejectKey(string value)
+    {
+        ((ConsoleDataRequest<TOut>)Build())
+            .RejectRequestMessage = value;
+
+        return this;
+    }
+}
