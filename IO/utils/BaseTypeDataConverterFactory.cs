835059f03f590c3d761e523848dd22fabfcbae45
rejchev <hendlerq@yandex.ru>
2023-01-14 15:58:54 +0500

    arrow func be like

--- IO/utils/BaseTypeDataConverterFactory.cs
+++ IO/utils/BaseTypeDataConverterFactory.cs
@@ -5,46 +5,32 @@ namespace IO.utils;
 
 public static class BaseTypeDataConverterFactory
 {
-    public static ConsoleSimpleDataConverter<double> MakeSimpleDoubleConverter()
-    {
-        return ConsoleDataConverterFactory.MakeSimpleConverter<double>(double.TryParse);
-    }
+    public static ConsoleSimpleDataConverter<double> MakeSimpleDoubleConverter() => 
+        ConsoleDataConverterFactory.MakeSimpleConverter<double>(double.TryParse);
 
-    public static ConsoleSimpleDataConverter<int> MakeSimpleIntConverter()
-    {
-        return ConsoleDataConverterFactory.MakeSimpleConverter<int>(int.TryParse);
-    }
+    public static ConsoleSimpleDataConverter<int> MakeSimpleIntConverter() => 
+        ConsoleDataConverterFactory.MakeSimpleConverter<int>(int.TryParse);
 
-    public static ConsoleSimpleDataConverter<string> MakeSimpleStringConverter()
-    {
-        return ConsoleDataConverterFactory.MakeSimpleConverter(
-            ((string? data, out string output) => 
+    public static ConsoleSimpleDataConverter<string> MakeSimpleStringConverter() => 
+        ConsoleDataConverterFactory.MakeSimpleConverter((string? data, out string output) => 
             {
                 output = data ?? "";
                 return data != null;
-            })
+            }
         );
-    }
 
-    public static ConsoleSimpleDataConverter<float> MakeSimpleFloatConverter()
-    {
-        return ConsoleDataConverterFactory.MakeSimpleConverter<float>(float.TryParse);
-    }
+    public static ConsoleSimpleDataConverter<float> MakeSimpleFloatConverter() => 
+        ConsoleDataConverterFactory.MakeSimpleConverter<float>(float.TryParse);
 
-    public static ConsoleFormattedNumberDataConverter<double> MakeFormattedDoubleConverter(
-        NumberStyles styles = NumberStyles.Float | NumberStyles.AllowThousands,
-        IFormatProvider? provider = default)
-    {
-        return ConsoleDataConverterFactory.MakeFormattedNumberDataConverter<double>(double.TryParse, styles, provider);
-    }
+    public static ConsoleFormattedNumberDataConverter<double> 
+        MakeFormattedDoubleConverter(NumberStyles styles = NumberStyles.Float | NumberStyles.AllowThousands, 
+            IFormatProvider? provider = default) => ConsoleDataConverterFactory
+        .MakeFormattedNumberDataConverter<double>(double.TryParse, styles, provider);
 
-    public static ConsoleDataConverterList<double> MakeDoubleConverterList()
-    {
-        return ConsoleDataConverterFactory.MakeConverterList(
-            new List<IConvertibleData<string?, double>>
-            {
+    public static ConsoleDataConverterList<double> MakeDoubleConverterList() => 
+        ConsoleDataConverterFactory.MakeConverterList(new List<IConvertibleData<string?, double>> 
+        {
                 MakeSimpleDoubleConverter(),
                 MakeFormattedDoubleConverter(provider: NumberFormatInfo.InvariantInfo)
-            });
-    }
+        });
 }
