9dfc19aee9b8f03a4cbca261f033ec06f2e77c72
rejchev <hendlerq@yandex.ru>
2023-01-18 15:18:49 +0500

    skeleton of tests tasks of labs

--- labsTests/lab1/Task5.cs
+++ labsTests/lab1/Task5.cs
@@ -1,14 +1,20 @@
-using System.Numerics;
-using labs.lab1;
-
 namespace labsTests.lab1;
 
+// TODO: #
 public class Task5
 {
+    private labs.lab1.Task5 instance;
+
+    [SetUp]
+    public void Init() => 
+        instance = new labs.lab1.Task5("test", "test");
+
     [Test]
-    public void DotInTriangle()
-    {
-        Assert.True(labs.lab1.Task5.Triangle.Contains(new Vector<double>(
-            new []{ 0.0, -5.0 })));
-    }
+    public void DotInTriangle() => 
+        Assert.True(labs.lab1.Task5.TriangleContains((0.0, -5.0)));
+    
+    
+    [Test]
+    public void DotInCircle() => 
+        Assert.True(labs.lab1.Task5.CircleContains((10.0, 0.0)));
 }
---
+++ labsTests/lab9/Task1.cs
@@ -0,0 +1,12 @@
+namespace labsTests.lab9;
+
+
+// TODO: #
+public class Task1
+{
+    private labs.lab9.Task1 instance;
+
+    [SetUp]
+    public void Init() => 
+        instance = new labs.lab9.Task1("test", "test");
+}
