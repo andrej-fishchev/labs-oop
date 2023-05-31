a8fd093009c7ffd6c326e808d5900878776dc366
rejchev <hendlerq@yandex.ru>
2023-05-31 22:28:49 +0500

    use matrix to graph component search

--- labs/shared/data/algorithms/Graph/searches/GraphComponentSearcher.cs
+++ labs/shared/data/algorithms/Graph/searches/GraphComponentSearcher.cs
@@ -1,29 +1,64 @@
+using System.Collections;
 using labs.shared.data.abstracts.graph;
-using labs.shared.data.algorithms.Graph.walks;
-using labs.shared.data.algorithms.Graph.walks.linq;
 
 namespace labs.shared.data.algorithms.Graph.searches;
 
 public static class GraphComponentSearcher
 {
-    public static IEnumerable<List<GraphVertex>> Components(this abstracts.Graph graph)
+    public static IEnumerable<List<GraphVertex>> Components(this GraphMatrix matrix)
     {
-        if(graph.VertexCount == 0)
+        if(matrix.VertexCount == 0)
             yield break;
         
-        var list = new List<GraphVertex>();
-        for (var i = 0; i < graph.VertexCount; i++)
+        var table = PrepareFinalMatrix(matrix).GetBitMatrixContainer();
+
+        foreach (var componenta in GetValidColumns(
+                     table, matrix.GetVertexes().ToList(), matrix.VertexCount))
+            yield return componenta;
+    }
+
+    private static GraphMatrix PrepareFinalMatrix(GraphMatrix matrix)
+    {
+        var mtrs = new List<GraphMatrix> {matrix};
+        for(int i = 1; i < matrix.VertexCount; i++)
+            mtrs.Add(GraphMatrix.Multiply(mtrs[i - 1], matrix)!);
+        
+        var output = GraphMatrix.E(matrix.GetVertexes());
+        for (int i = 0; i < mtrs.Count; i++)
+            output = GraphMatrix.Sum(output, mtrs[i]);
+
+        return GraphMatrix.Or(output, GraphMatrix.Transposition(output));
+    }
+
+    private static IEnumerable<List<GraphVertex>> GetValidColumns(BitArray[] table, List<GraphVertex> v, int size)
+    {
+        var line = new List<int>();
+
+        for (int i = 0; i < size; i++)
         {
-            if(list.Contains(graph.Adjacency[i].Vertex))
+            for (int j = 0; j < size; j++)
+            {
+                if(table[i][j])
+                    line.Add(j);
+            }
+                
+            if(line.Count == 0)
                 continue;
 
-            var dfsResult = graph.Walk(
-                new DepthFirstWalk(), graph.Adjacency[i].Vertex.ShortName
-            ).ToList();
+            yield return line.Select(x => v[x]).ToList();
+
+            table = ExcludeVertexes(table, line, size);
             
-            yield return dfsResult;
-            
-            list.AddRange(dfsResult);
+            line.Clear();
         }
+    }
+
+    private static BitArray[] ExcludeVertexes(BitArray[] table, IReadOnlyList<int> vertexes, int size)
+    {
+        foreach (var t in vertexes)
+            for (int j = 0; j < size; j++)
+                table[t][j] = false;
+
+        return table;
     }
 }
